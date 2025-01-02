d9eac8dde1082652f8a74df4bfec068c68668a4e
monority <chenu.pro@gmail.com>
2025-01-02 13:54:57 +0100

    Add .NET folder

---
+++ _.NET/Dev C# BASE/demos/demo01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+*Commentaire
+* sur
+* plusieurs lignes
+* Pour commenter/décommenter rapidement une ligne de code : 'ctrl + :'
+* Pour commenter plusieurs lignes : 'ctrl + maj + :'
+*/
+
+/*
+    Les conventions de nommages :
+    screaming snake case : SCREAMING_SNAKE_CASE
+    camel case : camelCase
+    snake case : snake_case
+    kebab case : kebab-case
+ */
+
+Console.WriteLine("Bonjour, je m'appelle Christophe");
+Console.WriteLine('\'');
+Console.WriteLine("C:\\Users\\Administrateur\\Desktop\\01 - Csharp bases");
+Console.WriteLine(@"C:\Users\Administrateur\Desktop\01 - Csharp bases");
---
+++ _.NET/_C#/exercices/exercice01/exercice01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("*** Ma liste de todo ***");
+Console.WriteLine("Aujourd'hui je dois faire:\n" +
+    "   - Apprendre le C# \n" +
+    "   - Apprendre à utiliser Visual Studio\n" +
+    "   - Comprendre l'affichage \"Console\"\n" +
+    "   - Créer mon répetoire C:\\MesExercices\\\" pour les ranger \n"+
+    "   - Apprécier les fonctionnalités du C#");
+
+
+Console.WriteLine("okok \n ok ");
+Console.WriteLine("okok" + "\n" + "ok");
