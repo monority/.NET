a471cb79ae9d86c5e0373bb50ee771cbd8e5683b
monority <chenu.pro@gmail.com>
2025-01-06 12:23:59 +0100

    NEW : exercice c#

---
+++ _.NET/_C#/exercices/exerciceCSharp/exercice15/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Drinks avaible : \n " +
+              "1. Water" + "\n"  +
+              "2. Sparkling WATER " + "\n"  +
+              "3. Coca " + "\n"  + 
+              "4. Fanta" + "\n"  + 
+              "5. Sprite " + "\n"  + 
+              "6. Orangina" + "\n"  + 
+              "7. 7UP \n") ;
+Console.WriteLine("Your choice ? 1 to 7");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+switch (choice)
+{
+    case 1: Console.WriteLine("You took Water"); break;
+    case 2: Console.WriteLine("You took Sparkling"); break;
+    case 3: Console.WriteLine("You took Coca"); break;
+    case 4: Console.WriteLine("You took Fanta"); break;
+    case 5: Console.WriteLine("You took Sprite"); break;
+    case 6: Console.WriteLine("You took Orangina"); break;
+    case 7: Console.WriteLine("You took 7UP"); break;
+    default : Console.WriteLine("Invalid choice"); break;
+}
---
+++ _.NET/_C#/exercices/exerciceCSharp/exercice16/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Enter your child age");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+switch (choice)
+{
+    
+    case >= 3 and <= 6 : Console.WriteLine("Baby"); break;
+    case >= 7 and <= 8 : Console.WriteLine("Poussin"); break;
+    case >= 9 and <= 10 : Console.WriteLine("Pupill"); break;
+    case >= 11 and <= 12 : Console.WriteLine("Minim"); break;
+    case >= 13 : Console.WriteLine("Cadet"); break;
+}
