825c42ecd52e8e4fce0361cd427098c11eea3876
Ronan C <126070278+monority@users.noreply.github.com>
2025-01-08 16:20:29 +0100

    NEW : Bookmark

--- _.NET/_C#/exercices/exerciceCSharp/exercice29/Program.cs
+++ _.NET/_C#/exercices/exerciceCSharp/exercice29/Program.cs
@@ -6,17 +6,9 @@ int input = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[input];
 Random random = new Random();
 Console.ForegroundColor = ConsoleColor.White;
-
 for (int i = 0; i < array.Length; i++)
 {
     int random_number = random.Next(0, 50);
     array[i] = random_number;
-    if (array[i] % 2 == 0)
-    {
-        Console.WriteLine($"Value {array[i]} is even");
-    }
-    else
-    {
-        Console.WriteLine($"Value {array[i]} is odd");
-    }
+    Console.WriteLine($"Number {array[i]} is {(array[i] % 2 == 0 ? "even" : "odd")}");
 }
--- _.NET/_C#/exercices/exerciceCSharp/exercice29/obj/Debug/net9.0/exercice29.AssemblyInfo.cs
+++ _.NET/_C#/exercices/exerciceCSharp/exercice29/obj/Debug/net9.0/exercice29.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercice29")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6db89369e873e920cf0e5e9f73e3ad96f125f5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4912e36c4bb1e1019fe91c904e162b6e3f9b7710")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercice29")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercice29")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
