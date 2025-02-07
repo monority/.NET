01619db58bc33de8eaea2042509ec5038c1ad866
Ronan C <126070278+monority@users.noreply.github.com>
2025-02-07 13:48:10 +0100

    INIT : Project Towns

---
+++ _test/Exercice03-Towns/TestProject1/TownsTest.cs
@@ -0,0 +1,12 @@
+namespace TestProject1;
+
+using System.Collections.ObjectModel;
+using Towns.Library;
+public class TownsTest
+{
+	[Fact]
+	public void CountChar_LessThan2()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ _test/Exercice03-Towns/Towns.Library/SearchTown.cs
@@ -0,0 +1,19 @@
+﻿
+using Towns.Library;
+
+namespace Towns.Library
+{
+	public class SearchTown
+	{
+		private List<String> _towns;
+
+		public List<String> Search(String word)
+		{
+
+			throw new NotImplementedException();
+
+		}
+	}
+}
+
+
