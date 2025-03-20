fdcfa86d09fcbf8f2c670d6705a09390b50f726f
Ronan C <126070278+monority@users.noreply.github.com>
2025-03-20 13:41:28 +0100

    UPDATE : Exercice Weather

--- _maui/maui-sln/Exercice05/Converters/WeatherImageConverter.cs
+++ _maui/maui-sln/Exercice05/Converters/WeatherImageConverter.cs
@@ -1,50 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Diagnostics;
-
+using SkiaSharp.Extended.UI.Controls;
+using Microsoft.Maui.Controls;
 
 namespace Exercice05.Converters
 {
     class WeatherImageConverter : IValueConverter
     {
-
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value == null)
             {
                 return null;
             }
-            string weather = value.ToString();  
-            string icon = string.Empty;
+
+            string weather = value.ToString();
             Debug.WriteLine(weather);
-            switch (weather)
+
+            var lottieView = new SKLottieView
             {
-                case "0":
-                    icon = "sunny.json";
-                    break;
-                case "1":
-                    icon = "foggy.json";
-                    break;
-                case "2":
-                    icon = "parly-cloudy.json";
-                    break;
-                case "3":
-                    icon = "windy.json";
-                    break;
-                default:
-                    icon = "sunny.json";
-                    break;
-            }
-            Debug.WriteLine(icon);
-            return icon;
+                RepeatCount = -1, // Repeat indefinitely
+                RepeatMode = SKLottieRepeatMode.Restart,
+                Source = GetPictureFromCode(weather)
+            };
+
+            return lottieView.Source;
         }
+
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
+
+        private SKLottieImageSource GetPictureFromCode(string weatherCode)
+        {
+            return new SKFileLottieImageSource
+            {
+                File = weatherCode switch
+                {
+                    "0" => "sunny.json",
+                    "1" => "foggy.json",
+                    "2" => "partly-cloudy.json",
+                    "3" => "windy.json",
+                    _ => "sunny.json"
+                }
+            };
+        }
     }
 }
+
